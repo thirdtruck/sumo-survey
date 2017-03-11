5d2b7b0f571bd08e7f9612c0ec4204c1d1b0ea5b
James Holder <github.com@thirdtruck.org>
2017-03-11 18:09:39 -0500

    Fetch or create Guest session before retrieving survey question.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
+var uuidV4 = require('uuid/v4');
 var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Question.find({
-    include: [{ model: models.Choice }],
-    order: [ models.sequelize.fn('RANDOM') ]
+  var promise;
+
+  if (req.session.uuid) {
+    promise = models.Guest.find({
+      where: { sessionId: req.session.uuid }
+    })
+  } else {
+    req.session.uuid = uuidV4();
+
+    promise = models.Guest.create({
+      sessionId: req.session.uuid
+    });
+  }
+
+  promise.then(function(guest) {
+    return models.Question.find({
+      include: [{ model: models.Choice }],
+      order: [ [ models.sequelize.fn('RANDOM') ] ]
+    })
   })
   .then(function(question) {
     return {
