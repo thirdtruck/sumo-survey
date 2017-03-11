06daf31bfd8c3ed38e2f69a1efaa2d1a3ff7cdb5
James Holder <github.com@thirdtruck.org>
2017-03-11 15:40:55 -0500

    Prototype Sequelize model usage.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Question.create({
+    title: "A Question"
+  })
+  .then(
+    function(question) {
+      models.Question.findAll()
+      .then(function(questions) {
+        res.render('index', {
+          question: { title: question.get('title') }
+        });
+      })
+    },
+    function(error) {
+      res.render('error', {
+        error: error,
+        message: "Unable to create Question."
+      })
+    }
+  )
 });
 
 module.exports = router;
