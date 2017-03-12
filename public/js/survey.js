c4f2937735c1b0b65f7ee8e8a036bb3d8ce1e4c9
James Holder <github.com@thirdtruck.org>
2017-03-11 21:52:49 -0500

    Add survey.js.

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,1 +1,18 @@
-console.log('survey.js');
+$(document).ready(function() {
+  var $question = $('#question');
+  $question.find('.choice').each(function(index, choice) {
+    var $choice = $(choice);
+
+    $choice.click(function() {
+      $.post('/response',
+        {
+          questionId: $question.data('question-id'),
+          choiceId: $choice.data('choice-id')
+        },
+        function(data) {
+          console.log("Response: ", data);
+        }
+      );
+    });
+  });
+});
