984893bde3c335838fc286002a1741a2952da2a4
James Holder <github.com@thirdtruck.org>
2017-03-12 16:16:11 -0400

    Prototype response posting.

--- routes/response.js
+++ routes/response.js
@@ -1,9 +1,22 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* POST response . */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.Guest.find({
+    where: { sessionId: req.session.uuid }
+  })
+  .then(function(guest) {
+    return models.Response.create({
+      GuestId: guest.id,
+      QuestionId: req.body.questionId,
+      ChoiceId: req.body.choiceId
+    });
+  })
+  .then(function(response) {
+    res.render('index');
+  });
 });
 
 module.exports = router;
