537b2e94790139ddb562243a97dc890c666a07c9
James Holder <github.com@thirdtruck.org>
2017-03-11 22:00:09 -0500

    Show status messages. Show link to answer another question after answering one.

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   var $question = $('#question');
+  var $choices = $('#choices');
+  var $answerAnother = $('#answer-another');
+
   $question.find('.choice').each(function(index, choice) {
     var $choice = $(choice);
 
     $choice.click(function() {
+      $choices.replaceWith("Submitting ...");
+
       $.post('/response',
         {
           questionId: $question.data('question-id'),
           choiceId: $choice.data('choice-id')
         },
         function(data) {
-          console.log("Response: ", data);
+          $question.replaceWith('Answer submitted.');
+          $answerAnother.show();
         }
       );
     });
