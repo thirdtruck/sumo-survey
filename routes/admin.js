b7f0401d7bd8d26046b0a66863b6bb79936309e9
James Holder <github.com@thirdtruck.org>
2017-03-12 21:46:01 -0400

    Initialize /admin/add-question route.

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,10 @@ router.get('/', basicAuthentication, function(req, res, next) {
   res.render('admin');
 });
 
+router.get('/add-question', basicAuthentication, function(req, res, next) {
+  res.render('admin-add-question');
+});
+
 router.get('/stats', basicAuthentication, function(req, res, next) {
   models.Question.findAll({
     include: [
