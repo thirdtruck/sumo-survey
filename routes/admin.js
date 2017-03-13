e673b0a73fe07021e9fda53b18b1fce85957d9ca
James Holder <github.com@thirdtruck.org>
2017-03-12 21:25:25 -0400

    Prototype by-question stats table.

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 var basicAuthentication = require('../lib/authentication').basicAuthentication;
@@ -8,7 +9,32 @@ router.get('/', basicAuthentication, function(req, res, next) {
 });
 
 router.get('/stats', basicAuthentication, function(req, res, next) {
-  res.render('admin-stats');
+  models.Question.findAll({
+    include: [
+      { model: models.Choice, include: models.Response }
+    ],
+    order: 'Question.id ASC'
+  })
+  .then(function(questions) {
+    var by_question = questions.map(function(question) {
+      var choices = question.Choices.map(function(choice) {
+        console.log(choice.Responses);
+        return {
+          id: choice.id,
+          text: choice.text,
+          count: choice.Responses.length
+        };
+      });
+
+      return {
+        id: question.id,
+        title: question.title,
+        choices: choices
+      };
+    });
+
+    res.render('admin-stats', { by_question: by_question });
+  })
 });
 
 module.exports = router;
