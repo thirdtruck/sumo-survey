2436719022f9e772aef1822debc9c8360468ed00
James Holder <github.com@thirdtruck.org>
2017-03-12 20:53:12 -0400

    Add basic authentication.

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var basicAuthentication = require('../lib/authentication').basicAuthentication;
 
 /* GET admin page */
-router.get('/', function(req, res, next) {
+router.get('/', basicAuthentication, function(req, res, next) {
   res.render('admin');
 });
 
