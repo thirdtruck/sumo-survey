3c290fe3999b49e63a5421b74f0959fe5066611f
James Holder <github.com@thirdtruck.org>
2017-03-18 17:34:25 -0400

    Apply eslint feedback to models/response.js.

--- models/response.js
+++ models/response.js
@@ -1,17 +1,18 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Response = sequelize.define('Response', {
-    GuestId: DataTypes.UUID
+function exports(sequelize, DataTypes) {
+  const Response = sequelize.define('Response', {
+    GuestId: DataTypes.UUID,
   }, {
     classMethods: {
-      associate: function(models) {
+      associate(models) {
         // associations can be defined here
 
         models.Response.belongsTo(models.Guest, { as: 'Response', foreignKey: 'GuestId' });
         models.Response.belongsTo(models.Question, { as: 'Response', foreignKey: 'QuestionId' });
         models.Response.belongsTo(models.Choice, { as: 'Response', foreignKey: 'ChoiceId' });
-      }
-    }
+      },
+    },
   });
   return Response;
-};
+}
+
+module.exports = exports;
