3f74bdeeac83ff89763df8fa3264975362fc1410
James Holder <github.com@thirdtruck.org>
2017-03-18 19:11:55 -0400

    Apply eslint feedback to routes/admin.js.

--- routes/admin.js
+++ routes/admin.js
@@ -1,67 +1,92 @@
-var models = require('../models');
-var express = require('express');
-var router = express.Router();
-var basicAuthentication = require('../lib/authentication').basicAuthentication;
+const models = require('../models');
+const express = require('express');
+const basicAuthentication = require('../lib/authentication').basicAuthentication;
+
+const router = express.Router();
+
+function onGetIndex(req, res) {
+  res.render('admin');
+}
 
 /* GET admin page */
-router.get('/', basicAuthentication, function(req, res, next) {
-  res.render('admin');
-});
+router.get('/', basicAuthentication, onGetIndex);
 
-router.get('/add-question', basicAuthentication, function(req, res, next) {
+/* GET admin/add-question page */
+
+function onGetAddQuestion(req, res) {
   res.render('admin-add-question');
-});
+}
 
-router.post('/add-question', basicAuthentication, function(req, res, next) {
-  var questionTitle = req.body.questionTitle;
-  var choices = req.body['choices[]'];
+router.get('/add-question', basicAuthentication, onGetAddQuestion);
 
-  if (typeof(choices) == 'string') { // Occurs when only one choice is submitted
+/* POST admin/add-question page */
+
+function onPostQuestion(req, res) {
+  const questionTitle = req.body.questionTitle;
+  let choices = req.body['choices[]'];
+
+  if (typeof choices === 'string') { // Occurs when only one choice is submitted
     choices = [choices];
+  }
+
+  function mapTextToJSON(text) {
+    return { text };
+  }
+
+  function renderPage() {
+    res.render('admin-add-question');
+  }
+
+  function onError(error) {
+    res.render('error', { error });
   }
 
   models.Question.create({
     title: questionTitle,
-    Choices: choices.map(function(text) { return { text: text } })
+    Choices: choices.map(mapTextToJSON),
   }, {
-    include: models.Choice
+    include: models.Choice,
   })
-  .then(function() {
-    res.render('admin-add-question');
-  })
-  .catch(function(error) {
-    res.render('error', {
-      error: error
-    });
-  });
-});
+  .then(renderPage)
+  .catch(onError);
+}
 
-router.get('/stats', basicAuthentication, function(req, res, next) {
+router.post('/add-question', basicAuthentication, onPostQuestion);
+
+/* GET admin/stats page */
+
+function mapChoiceStatsToJSON(choice) {
+  return {
+    id: choice.id,
+    text: choice.text,
+    count: choice.Responses.length,
+  };
+}
+
+function mapQuestionStatsToJSON(question) {
+  const choices = question.Choices.map(mapChoiceStatsToJSON);
+
+  return {
+    id: question.id,
+    title: question.title,
+    choices,
+  };
+}
+
+function getStats(req, res) {
+  function renderStats(questions) {
+    const byQuestion = questions.map(mapQuestionStatsToJSON);
+
+    res.render('admin-stats', { by_question: byQuestion });
+  }
+
   models.Question.findAll({
-    include: [
-      { model: models.Choice, include: models.Response }
-    ],
-    order: 'Question.id ASC'
+    include: [{ model: models.Choice, include: models.Response }],
+    order: 'Question.id ASC',
   })
-  .then(function(questions) {
-    var by_question = questions.map(function(question) {
-      var choices = question.Choices.map(function(choice) {
-        return {
-          id: choice.id,
-          text: choice.text,
-          count: choice.Responses.length
-        };
-      });
+  .then(renderStats);
+}
 
-      return {
-        id: question.id,
-        title: question.title,
-        choices: choices
-      };
-    });
-
-    res.render('admin-stats', { by_question: by_question });
-  })
-});
+router.get('/stats', basicAuthentication, getStats);
 
 module.exports = router;
