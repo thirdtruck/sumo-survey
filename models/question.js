3e26557ead043c2b0dd52dfea561cec9450655e5
James Holder <github.com@thirdtruck.org>
2017-03-18 17:35:20 -0400

    Apply eslint feedback to models/question.js.

--- models/question.js
+++ models/question.js
@@ -1,16 +1,17 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Question = sequelize.define('Question', {
-    title: DataTypes.TEXT
+function exports(sequelize, DataTypes) {
+  const Question = sequelize.define('Question', {
+    title: DataTypes.TEXT,
   }, {
     classMethods: {
-      associate: function(models) {
+      associate(models) {
         // associations can be defined here
 
         models.Question.hasMany(models.Choice);
         models.Question.hasMany(models.Response);
-      }
-    }
+      },
+    },
   });
   return Question;
-};
+}
+
+module.exports = exports;
