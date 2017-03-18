d17a64e95acf9e5f05c3c5802a9c5213aff62c1a
James Holder <github.com@thirdtruck.org>
2017-03-18 18:05:13 -0400

    Apply eslint feedback to routes/response.js.

--- routes/response.js
+++ routes/response.js
@@ -1,22 +1,27 @@
-var models = require('../models');
-var express = require('express');
-var router = express.Router();
+const models = require('../models');
+const express = require('express');
 
-/* POST response . */
-router.post('/', function(req, res, next) {
-  models.Guest.find({
-    where: { sessionId: req.session.uuid }
-  })
-  .then(function(guest) {
+const router = express.Router();
+
+function onPostResponse(req, res) {
+  function createResponse(guest) {
     return models.Response.create({
       GuestId: guest.id,
       QuestionId: req.body.questionId,
-      ChoiceId: req.body.choiceId
+      ChoiceId: req.body.choiceId,
     });
-  })
-  .then(function(response) {
+  }
+
+  function renderResponse() {
     res.render('index');
-  });
-});
+  }
+
+  models.Guest.find({ where: { sessionId: req.session.uuid } })
+  .then(createResponse)
+  .then(renderResponse);
+}
+
+/* POST response . */
+router.post('/', onPostResponse);
 
 module.exports = router;
