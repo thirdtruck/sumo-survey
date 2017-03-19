7fffbc49526feabcb6b58c66f1c070bf5000e04c
James Holder <github.com@thirdtruck.org>
2017-03-18 22:51:47 -0400

    Revert "Apply eslint feedback to models/index.js."
    This reverts commit 3a288d1c0641655900c2f392d65a0132f99395ed.

--- models/index.js
+++ models/index.js
@@ -1,42 +1,34 @@
-const env = process.env.NODE_ENV || 'development';
+'use strict';
 
-const config = require('../config/config')[env];
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-
-let sequelize;
-
-const basename = path.basename(module.filename);
-const db = {};
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-function onlyJSFiles(file) {
-  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-}
-
-function importModel(file) {
-  const model = sequelize.import(path.join(__dirname, file));
-  db[model.name] = model;
-}
-
-function assignAssociations(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(onlyJSFiles)
-  .forEach(importModel);
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(function(file) {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
-Object.keys(db).forEach(assignAssociations);
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
