af2fa7e92da4797af1f7a45562ba4337a6a114b2
James Holder <github.com@thirdtruck.org>
2017-03-18 22:51:57 -0400

    Revert "Apply eslint feedback to app.js."
    This reverts commit 43d4243ba5b7ebafb43f23fed4a77289d463ee56.

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const response = require('./routes/response');
-const admin = require('./routes/admin');
+var index = require('./routes/index');
+var response = require('./routes/response');
+var admin = require('./routes/admin');
 
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
-  secret: 'secret string',
+  secret: 'secret string'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,26 +33,21 @@ app.use('/response', response);
 app.use('/admin', admin);
 
 // catch 404 and forward to error handler
-function on404(req, res, next) {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
-}
-
-app.use(on404);
+});
 
 // error handler
-function onError(err, req, res) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  const locals = res.locals;
-  locals.message = err.message;
-  locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-}
-
-app.use(onError);
+});
 
 module.exports = app;
