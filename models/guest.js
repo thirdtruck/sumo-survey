08929f96e229b136e84fc7bd28f105c5e189271d
James Holder <github.com@thirdtruck.org>
2017-03-18 22:49:35 -0400

    Revert "Apply eslint feedback to models/guest.js."
    This reverts commit a7ccf88698f00c2b1d50aad14d0be51707dc216c.

--- models/guest.js
+++ models/guest.js
@@ -1,16 +1,15 @@
-function exports(sequelize, DataTypes) {
-  const Guest = sequelize.define('Guest', {
-    sessionId: DataTypes.UUID,
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Guest = sequelize.define('Guest', {
+    sessionId: DataTypes.UUID
   }, {
     classMethods: {
-      associate(models) {
+      associate: function(models) {
         // associations can be defined here
 
         models.Guest.hasMany(models.Response);
-      },
-    },
+      }
+    }
   });
   return Guest;
-}
-
-module.exports = exports;
+};
